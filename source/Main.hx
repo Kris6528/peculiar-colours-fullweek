0dbf98f506bbbe614223accc18f09e9c47b5391b
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-08-25 00:00:45 +0300

    u forgor add these

--- source/Main.hx
+++ source/Main.hx
@@ -39,6 +39,8 @@ class Main extends Sprite
 	{
 		super();
 
+                SUtil.uncaughtErrorHandler();
+
 		if (stage != null)
 		{
 			init();
@@ -81,12 +83,9 @@ class Main extends Sprite
 
 		addChild(game);
 		
-		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
-
-		#end
 	}
 
 	var game:FlxGame;
